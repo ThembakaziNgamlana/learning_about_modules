02ff93bcd7e201564f86ccca3749c7aa9bc437b2
Thembakazi <thembakazingamlana25@gmail.com>
2023-07-19 15:33:07 +0200

    adding changes

--- greet_xola.js
+++ greet_xola.js
@@ -1,17 +1,11 @@
-// import chalk from 'chalk';
 
-// import greet from './greet.js'
+import chalk from 'chalk';
 
-
-// const styledMessage = chalk.bgGreen.black(greet('Xola'));
-
-// console.log(styledMessage)
-
-// var figlet = require("figlet");
 
 import figlet from "figlet"
 
 import greet from './greet.js'
 const styledMessage = figlet.textSync(greet('Xola'));
 console.log(styledMessage)
+
 
